8f1e76d4650d00c83b9685148a8edeff438465d7
wilsonmike <michaelwilsonsj@hotmail.com>
2021-11-20 01:29:57 -0500

    added global styles && username slug routes

---
+++ lib/firebase.ts
@@ -0,0 +1,22 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA1BXWomWYOaxKvV26YeszfNZ8W4GyEGVo",
+  authDomain: "betfire-ac9ed.firebaseapp.com",
+  projectId: "betfire-ac9ed",
+  storageBucket: "betfire-ac9ed.appspot.com",
+  messagingSenderId: "674467181975",
+  appId: "1:674467181975:web:020f6d59f708d124c940d2",
+  measurementId: "G-XQYDTECYXE",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Link href={{ pathname: "/[username]", query: { username: "vimike" } }}>
+        <a>Vimike</a>
+      </Link>
+      <h2>vimike</h2>
+    </div>
+  );
 }
