bba66e0ad9d5566a2b9a88d0ab46543f46ac74db
wilsonmike <michaelwilsonsj@hotmail.com>
2021-11-20 22:55:24 -0500

    sign in with Google added -- only token

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,7 +1,40 @@
+import { auth, googleAuthProvider } from "../lib/firebase";
+
 export default function EnterPage({}) {
+  const user = null;
+  const username = null;
+
+  // user signed out <SignInButton />
+  // user signed in, but missing username <UserNameForm />
+  // user signed in, has username <SignOutButton />
   return (
     <main>
-      <h1>Sign Up</h1>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
     </main>
   );
 }
+
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await auth.signInWithPopup(googleAuthProvider);
+  };
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={"/google.png"} /> Sign In With Google
+    </button>
+  );
+}
+
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>Sign Out</button>;
+}
+
+function UsernameForm() {}
