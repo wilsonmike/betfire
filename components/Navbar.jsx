28bb7f516934bded828d3cbec6505063e319202d
wilsonmike <michaelwilsonsj@hotmail.com>
2021-11-20 22:29:46 -0500

    added navbar && navbar styles

---
+++ components/Navbar.jsx
@@ -0,0 +1,41 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable @next/next/link-passhref */
+import Link from "next/link";
+
+export default function Navbar() {
+  const user = null;
+  const username = null;
+
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link href="/">
+            <button className="btn-logo">FEED</button>
+          </Link>
+        </li>
+        {/* user is signed in and has username */}
+        {username && (
+          <>
+            <li className="push-left">
+              <Link href="/admin">
+                <button className="btn-blue">Write Posts</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${username}`}>
+                <img src={user?.photoURL} alt="User Avatar" />
+              </Link>
+            </li>
+          </>
+        )}
+        {/* user is not signed in OR has not created username */}
+        {!username && (
+          <Link href="/enter">
+            <button className="btn-blue">Log In</button>
+          </Link>
+        )}
+      </ul>
+    </nav>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,12 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
+import Loader from "../components/Loader";
 
 export default function Home() {
   return (
     <div>
-      <Link href={{ pathname: "/[username]", query: { username: "vimike" } }}>
-        <a>Vimike</a>
-      </Link>
-      <h2>vimike</h2>
+      <Loader show />
     </div>
   );
 }
